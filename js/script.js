37c06150729bcde98f1491f8d8f9649bdf8b0265
iCLIENTE <aloar.ferreira@aluno.senai.br>
2023-12-08 00:58:19 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 //efeito do botão voltar ao Topo
 
+function Topo(){
+    window.scrollTo(
+        {
+        top:0,
+        left:0,
+        behavior:'smooth'
+    }
+
+    )
+}
+
 //Validação de Login
 
-//Ativar alert no botão cadastrar
+function Login(){
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if(usuario == 'admim' && senha == '123456'){
+        window.location = "index.html"
+        logado = 1;
+    }
+
+    if(logado == 0){
+        alert("Acesso Negado. Dados incorretos");
+    }
+}
